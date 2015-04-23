c014433cb3cf440a470489c1b0e75904ece7fce6
Garrett Olson & Steve Schneider <student@epicodus.com>
2015-04-23 16:29:13 -0700

    add final allergy_checker logic, get unit tests passing

---
+++ lib/allergy_checker.rb
@@ -0,0 +1,28 @@
+class Fixnum
+  define_method(:allergies) do
+    target_score = self
+
+    output = []
+
+    allergens = {"cats"         => 128,
+                 "pollen"       => 64,
+                 "chocolate"    => 32,
+                 "tomatoes"     => 16,
+                 "strawberries" => 8,
+                 "shellfish"    => 4,
+                 "peanuts"      => 2,
+                 "eggs"         => 1}
+
+    until target_score.==(0)
+      allergens.each do |k1, v1|
+        if target_score >= (v1)
+          output.push(k1)
+          target_score = target_score.-(v1)
+        else
+
+        end
+      end
+    end
+    output
+  end
+end
--- spec/allergies_checker_spec.rb
+++ spec/allergies_checker_spec.rb
@@ -1,14 +1,14 @@
 require('capybara/rspec')
 require('allergy_checker')
 
-describe('Fixnum#allergy_checker') do
+describe('Fixnum#allergies') do
   it('returns an allergy when a number is inputted.') do
-    expect((1).allergy_checker()).to(eql("eggs"))
+    expect((1).allergies).to(eql(["eggs"]))
   end
-
-  
-
-
-
-
+  it('returns an allergy when a number higher than one is inputted.') do
+    expect((2).allergies).to(eql(["peanuts"]))
+  end
+  it('returns all allergies when a 255 is inputted.') do
+    expect((255).allergies).to(eql(["cats", "pollen", "chocolate", "tomatoes", "strawberries", "shellfish", "peanuts", "eggs"]))
+  end
 end
